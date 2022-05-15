7ca2bab64cb9cef323005fe620189e6395052de5
gozooit <gozoo.it@protonmail.com>
2022-05-15 16:53:17 +0200

    https://www.theodinproject.com/lessons/foundations-fundamentals-part-3 solved

--- javascript.js
+++ javascript.js
@@ -1,17 +1,32 @@
 function checkAge(age) {
     if (age > 18) {
-      return true;
+        return true;
     } else {
-      return confirm('Did parents allow you?');
+        return confirm('Did parents allow you?');
     }
-  }
+}
 
 function checkAge(age) {
     (age > 18) ? true : confirm('Did parents allow you?');
-    }
+}
 
 function checkAge(age) {
     return (age > 18) || confirm('Did parents allow you?');
-    }
+}
 
-checkAge(12);
+
+function add7(number) {
+    return number + 7;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function lastLetter(string) {
+    return string.charAt(string.length - 1);
+}
